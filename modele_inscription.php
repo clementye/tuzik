80c94793404b43497aa9d203ed2d327de5d900f3
Sébastien <slopez@et.intechinfo.fr>
2022-11-29 10:27:42 +0100

    Changement du script d'inscription

--- modele_inscription.php
+++ modele_inscription.php
@@ -1,30 +1,13 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>
-Page Inscription v.0.5
-</title>
-</head>
-<body>
-<form action="reponse.php" method="GET">
-  Prénom :<input type="text" name="nom">
-  Nom :<input type="text" name="">
-  Email :<input type="text" name="age">
-  Mot de passe :<input type="text" name="age">
-  Téléphone :<input type="text" name="age">
-  <br>
-  Cochez si vrai : <br>
-  <input type="radio" name="numMag">
-  <input type="radio" name="numFab">
-<p>
-<input type=submit value="Envoyer">
-</form>
-</body>
-</html>
 <?php
 
+  $nom = $_GET['nom'];
+  $nom = $_GET['prenom'];
+  $nom = $_GET['telephone'];
+  $nom = $_GET['mdp'];
+  $nom = $_GET['email'];
+
     require('connect.php');
+    require('inscription.html');
     $dsn="mysql:dbname=".BASE.";host=".SERVER;
     try{
       $connexion=new PDO($dsn,USER,PASSWD);
@@ -39,23 +22,24 @@ Page Inscription v.0.5
   else {
     foreach ($connexion->query($sql) as $row)
       if ($row['email'] == $email){
-        echo "Cet email fut déjà utilisé par un autre utilisateur. Vérifiez que l'email écrite est la bonne."
+        echo "Cet email fut déjà utilisé par un autre utilisateur. Vérifiez que l'email écrite est la bonne.";
+        echo "<script> location.href='inscription.html'; </script>";
+        die();
       }
   }
 
     $sql="INSERT INTO utilisateur ( `Nom`, `Prenom`, `telephone`, `email`,
-      `motdepasse`, `Num Magasin`, `Num Fabricant`)
-       VALUES (:nom, :prenom, :telephone, :email, :mdp, :numMag, :numFab)";
+      `motdepasse`)
+       VALUES (:nom, :prenom, :telephone, :email, :mdp)";
 
     $sql->execute(array(
       ':nom' => $nom,
       ':age' => $prenom,
       ':telephone' => $telephone,
       ':email' => $email,
-      ':mdp' => $mdp,
-      ':numMag' => $numMag,
-      ':numFab' => $numFab));
+      ':mdp' => $mdp,));
 
+      $sql = "SELECT nom, prenom FROM utilisateur WHERE email like :email";
       if(!$connexion->query($sql)) echo "Erreur serveur. Réessayer ulterieurement.";
     else {
       foreach ($connexion->query($sql) as $row)
