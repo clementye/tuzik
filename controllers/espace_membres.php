4b2ae65f41cb652005d914a49481bbfea01f1f4c
Sébastien <slopez@et.intechinfo.fr>
2022-12-06 10:37:34 +0100

    Modif dans : Authen, Espa_Memb et user. Créa : Modif_Uti

--- controllers/espace_membres.php
+++ controllers/espace_membres.php
@@ -1,5 +1,6 @@
 <?php
   if (isset($_SESSION["user-name"])) {
+    $déconnexion = "";
     require_once "../views/espace_personnel.php";
   } else {
     require_once "../views/connexion.php";
---
+++ controllers/modifier_utilisateur.php
@@ -0,0 +1,15 @@
+<?php
+  require_once "../models/utilisateur.php";
+
+  // On vérifie que l'email et
+  $changement = $recuperer_utilisateur($_POST[]);
+
+  if ($changement == false){
+    $erreur = "Erreur de connexion au serveur.";
+    require_once("/views/.php");
+  } else {
+    $modifier_utilisateur($_POST[]);
+    $confirmation = "Les changements furent effectués.";
+  }
+
+ ?>
