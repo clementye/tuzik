5b50763e23f411daea89fe6a2d762ec749c7d169
Sébastien <slopez@et.intechinfo.fr>
2022-12-14 11:41:23 +0100

    ?²

--- controllers/modifier_utilisateur.php
+++ controllers/modifier_utilisateur.php
@@ -7,14 +7,9 @@
     $erreur = "Erreur de connexion au serveur.";
     require_once "../views/modifier_utilisateur.php";
   } else {
-<<<<<<< HEAD
-    $modifier_utilisateur($_POST[]);
-    $confirmation = "Les changements ont été effectués.";
-=======
     $modifier_utilisateur($_POST["newPassword"], $_POST["nom"], $_POST["prenom"], $_POST["telephone"], $_POST["email"]);
     $confirmation = "Les changements furent effectués.";
     require_once "../controllers/espace_membres.php";
->>>>>>> Sébastien
   }
 
  ?>
