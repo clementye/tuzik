361d28fc5b1d0968d402cf41e437cd1555de60f1
Sébastien <slopez@et.intechinfo.fr>
2023-01-23 13:36:50 +0100

    Suppression if inutile

--- controllers/afficherMusicien.php
+++ controllers/afficherMusicien.php
@@ -1,10 +1,6 @@
 <?php
 
-session_start();
-  if (isset($_SESSION["user-name"])) {
-    require_once "../views/afficherMusicien.php";
-  } else {
-    require_once "../views/connexion.php";
-}
+  session_start();
+  require_once "../views/afficherMusicien.php";
 
 ?>
